2d9bd4348cf33044df60c21ded674b1bdd3d1a92
Jervx <louellagracechua@gmail.com>
2024-09-19 22:48:27 +0800

    chore: Initial setup for client

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+    return (
+        <div className="flex min-h-screen bg-stone-800 p-5 text-white">
+            <p>Jomoc</p>
+        </div>
+    );
+}
+
+export default App;
