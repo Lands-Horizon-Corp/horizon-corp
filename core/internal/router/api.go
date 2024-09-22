7d3f8a77bb9ac6f3e4f0f3106338e4704c3f9c39
Zalven <zalven.dayao@go.team>
2024-09-22 22:18:15 +0800

    Changing server

--- core/internal/router/api.go
+++ core/internal/router/api.go
@@ -4,7 +4,9 @@ import (
 	"horizon-core/config"
 	"horizon-core/internal/handler"
 	"horizon-core/internal/websocket"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
 	"gorm.io/gorm"
@@ -12,6 +14,28 @@ import (
 
 func ProvideAPI(lc fx.Lifecycle, cfg *config.Config, db *gorm.DB, hub *websocket.Hub) {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://0.0.0.0",
+			"http://0.0.0.0:8080",
+			"http://0.0.0.0:3000",
+			"http://0.0.0.0:3001",
+			"http://0.0.0.0:80",
+			"http://0.0.0.0:3000",
+			"http://rea.development",
+			"http://rea.pro",
+			"http://localhost:8080",
+			"http://localhost:3000",
+			"http://localhost:3001",
+			"http://localhost:3002",
+		},
+		AllowMethods:     []string{"POST", "GET", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	v1 := router.Group("/api/v1")
 	{
 		// Register handlers
