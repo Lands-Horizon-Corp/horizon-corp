ee61723e0e20001ba756ec21b4888e2673d903e7
Jervx <louellagracechua@gmail.com>
2024-09-23 16:59:00 +0800

    chore: Remove outlet in landing index page

--- client/src/components/modules/landing/pages/index.tsx
+++ client/src/components/modules/landing/pages/index.tsx
@@ -1,12 +1,9 @@
-import { Outlet } from '@tanstack/react-router'
-
 interface Props {}
 
 const LandingPage = (_props: Props) => {
     return (
         <div>
             Public
-            <Outlet />
         </div>
     )
 }
