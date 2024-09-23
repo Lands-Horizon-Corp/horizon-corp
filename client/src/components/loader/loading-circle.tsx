3518fe9b33d813c742e9aec21108ae7853caf335
Jervx <louellagracechua@gmail.com>
2024-09-23 14:41:49 +0800

    style(auth): Enhanced sign up page & form

---
+++ client/src/components/loader/loading-circle.tsx
@@ -0,0 +1,13 @@
+import { AiOutlineLoading } from 'react-icons/ai'
+
+import { cn } from '@/lib/utils'
+
+import { IBaseCompNoChild } from '@/interfaces'
+
+interface Props extends IBaseCompNoChild {}
+
+const LoadingCircle = ({ className }: Props) => {
+    return <AiOutlineLoading className={cn("size-4 animate-spin [animation-duration:.5s]", className)}/>
+}
+
+export default LoadingCircle
---
+++ client/src/components/modules/auth/validations/sign-up-form.ts
@@ -0,0 +1,65 @@
+import z from 'zod'
+
+import { PASSWORD_MIN_LENGTH, LETTERS_REGEX } from '../constants'
+import { AuthModeSchema } from '.'
+
+export const signUpFormSchema = z
+    .object({
+        email: z
+            .string({ required_error: 'Email is required' })
+            .email('Email must be valid'),
+        username: z
+            .string({ required_error: 'Username is required' })
+            .min(1, 'User Name is required'),
+        firstName: z
+            .string({ required_error: 'First Name is required' })
+            .min(1, 'First Name too short')
+            .regex(LETTERS_REGEX, 'First Name must contain only letters'),
+        middleName: z
+            .string()
+            .transform((val) => val || undefined)
+            .optional()
+            .refine(
+                (val) => {
+                    if (!val || val === '') return true
+                    return LETTERS_REGEX.test(val)
+                },
+                {
+                    message: 'First Name must contain only letters',
+                }
+            ),
+        lastName: z
+            .string({ required_error: 'Last Name is required' })
+            .min(1, 'Last Name is required')
+            .regex(LETTERS_REGEX, 'Last Name must contain only letters'),
+        contactNumber: z
+            .string()
+            .min(1)
+            .max(11)
+            .regex(/^\d+$/, 'Contact number must contain only numbers'),
+        password: z
+            .string({ required_error: 'Password is required' })
+            .min(
+                PASSWORD_MIN_LENGTH,
+                `Password must atleast ${PASSWORD_MIN_LENGTH}`
+            ),
+        confirmPassword: z
+            .string({ required_error: 'Confirm password' })
+            .min(PASSWORD_MIN_LENGTH, `Password doesn't match`),
+        acceptTerms: z
+            .boolean()
+            .default(false)
+            .refine(
+                (val) => {
+                    return val === true
+                },
+                {
+                    message: 'You must accept the terms and conditions',
+                }
+            ),
+        mode: AuthModeSchema,
+    })
+    .refine(({ password, confirmPassword }) => password === confirmPassword, {
+        message: "Password doesn't match",
+        path: ['confirm_password'],
+    })
