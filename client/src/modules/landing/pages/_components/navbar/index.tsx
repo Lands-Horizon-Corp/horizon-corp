a1381a06e4ba535440c38d9c14071aa82d64d1ef
rojan <yepesrojan@gmail.com>
2024-10-02 11:34:34 +0800

    HCOP-1: Landing Pages
    What:
    
      - HCOP-1 Home Page, Contact Page, About Page UI
    
    Why:
    
      - As per HCOP user can navigate Home Page, Contact Page and Abou Page.

--- client/src/modules/landing/pages/_components/navbar/index.tsx
+++ client/src/modules/landing/pages/_components/navbar/index.tsx
@@ -3,29 +3,78 @@ import { Link } from '@tanstack/react-router'
 import ecoop_logo from '@/assets/images/ecoop_logo.png'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { TbExternalLink } from 'react-icons/tb'
+import { ReactNode, useEffect, useState } from 'react'
+import { useRouter } from '@tanstack/react-router'
+
+type NavLink = {
+    name: string
+    path: string
+    icon?: ReactNode
+}
+
+const navLinks: NavLink[] = [
+    {
+        name: 'Home',
+        path: '/',
+    },
+    {
+        name: 'About',
+        path: '/about',
+    },
+    {
+        name: 'Contact',
+        path: '/contact',
+    },
+    {
+        name: 'Developers',
+        path: '/dev',
+        icon: <TbExternalLink />,
+    },
+]
 
 const NavBar = ({}: Props) => {
+    const router = useRouter()
+    const currentPath = router.state.location.pathname
+
+    const [currentTab, setCurrentTab] = useState<string>(currentPath)
+
+    useEffect(() => {
+        setCurrentTab(currentPath)
+    }, [])
+
     return (
         <>
             <nav className="flex h-[68px] justify-between gap-x-2 px-4 lg:px-16">
                 <div className="flex w-fit items-center justify-center">
-                    <img
-                        src={ecoop_logo}
-                        className="h-auto w-[46px]"
-                        alt="Logo"
-                    />
+                    <Link to="/">
+                        <img
+                            src={ecoop_logo}
+                            className="h-auto w-[46px] cursor-pointer"
+                            alt="Logo"
+                        />
+                    </Link>
                 </div>
                 <div className="flex h-full items-center justify-center text-[15px]">
                     <div className="flex space-x-[14px] font-medium">
-                        <Link className="" to="/">
-                            <p className="scale-effects nav-links">Home</p>
-                        </Link>
-                        <Link to="/about">
-                            <p className="scale-effects nav-links">About</p>
-                        </Link>
-                        <Link to="/contact">
-                            <p className="scale-effects nav-links">Contact</p>
-                        </Link>
+                        {navLinks.map((link, index) => (
+                            <div
+                                key={index}
+                                className="relative flex space-x-1"
+                            >
+                                <Link
+                                    className="scale-effects nav-links"
+                                    onClick={() => setCurrentTab(link.path)}
+                                    to={link.path}
+                                >
+                                    {link.name}
+                                </Link>
+                                <div className="self-center">{link.icon}</div>
+                                {currentTab === link.path && (
+                                    <div className="absolute -bottom-2 h-[5px] w-[20px] rounded-full bg-black"></div>
+                                )}
+                            </div>
+                        ))}
                     </div>
                 </div>
                 <div className="flex h-full items-center justify-center">
