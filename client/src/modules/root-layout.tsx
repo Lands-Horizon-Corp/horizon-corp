0818fb2acf8b1ee80727acbf979026dabb2d2952
rojan <dctrrohan@gmail.com>
2024-10-04 20:12:55 +0800

    feat: WIP Feedback and Versioning

--- client/src/modules/root-layout.tsx
+++ client/src/modules/root-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from '@tanstack/react-router'
 import { Toaster } from '@/components/ui/sonner'
+import { VersionAndFeedBack } from '@/components/version'
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className="relative">
+            <VersionAndFeedBack />
             <Outlet />
             <Toaster richColors />
         </div>
