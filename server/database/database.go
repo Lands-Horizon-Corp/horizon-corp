213185c9111501ea999951b149825851d48bcc73
Zalven <zalven.dayao@go.team>
2024-10-05 15:02:51 +0800

    feat: Creating document docker file

--- server/database/database.go
+++ server/database/database.go
@@ -1,52 +1,3 @@
-// package database
-
-// import (
-// 	"fmt"
-// 	"horizon/server/config"
-
-// 	"go.uber.org/zap"
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// )
-
-// type DB struct {
-// 	*gorm.DB
-// 	Logger *zap.Logger
-// }
-
-// func NewDB(cfg *config.AppConfig, logger *zap.Logger) (*DB, error) {
-// 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%t&loc=%s",
-// 		cfg.DBUsername,
-// 		cfg.DBPassword,
-// 		cfg.DBHost,
-// 		cfg.DBPort,
-// 		cfg.DBName,
-// 		cfg.DBCharset,
-// 		cfg.DBParseTime,
-// 		cfg.DBLoc,
-// 	)
-
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	logger.Info("Database connection established")
-// 	return &DB{db, logger}, nil
-// }
-
-// func (db *DB) Close() {
-// 	if db.DB != nil {
-// 		sqlDB, err := db.DB.DB()
-// 		if err != nil {
-// 			db.Logger.Fatal("Error getting database from GORM", zap.Error(err))
-// 		}
-// 		if err := sqlDB.Close(); err != nil {
-// 			db.Logger.Fatal("Error closing the database", zap.Error(err))
-// 		}
-// 	}
-// }
-
 package database
 
 import (
@@ -78,7 +29,6 @@ func NewDB(cfg *config.AppConfig) (*gorm.DB, error) {
 	return nil, fmt.Errorf("failed to connect to database after %d attempts: %w", maxRetries, err)
 }
 
-// buildDSN constructs the data source name (DSN) for the database connection.
 func buildDSN(cfg *config.AppConfig) string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
 		cfg.DBUsername,
@@ -92,7 +42,6 @@ func buildDSN(cfg *config.AppConfig) string {
 	)
 }
 
-// tryConnect attempts to open a connection to the database and returns the database object and error.
 func tryConnect(dsn string) (*gorm.DB, error) {
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
