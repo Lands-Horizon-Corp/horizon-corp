4027001f341345a5d2d0b20797fc72db2cd84ca9
Zalven <zalven.dayao@go.team>
2024-10-05 22:47:57 +0800

    feat: remove dotenv

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { config } from 'dotenv';
 // import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 import tsconfigPaths from "vite-tsconfig-paths";
 
-config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
