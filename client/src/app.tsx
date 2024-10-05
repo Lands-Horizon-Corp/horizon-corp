93b4f339d787b50c97623362040750ea4a08c6d8
Zalven <zalven.dayao@go.team>
2024-10-05 19:27:22 +0800

    feat: Adding test connection to backend and frontend

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from '@tanstack/react-router';
 
 import router from '@/root-route';
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import useErrorDetailsState from './states/error-details-state';
 
 const TanStackRouterDevtoolsPanel =
     process.env.NODE_ENV === 'production'
@@ -20,6 +21,16 @@ declare module '@tanstack/react-router' {
 }
 
 const App = () => {
+  const { connection } = useErrorDetailsState(); 
+  useEffect(() => {
+    const checkConnection = async () => {
+      const isConnected = await connection();
+      // TODO: Add network error page if the frontend is not connected to backend
+      console.log('Connection status:', isConnected);
+    };
+
+    checkConnection();
+  }, [connection]);
     return (
         <ThemeProvider>
             <RouterProvider router={router} />
