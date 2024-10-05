d1a946e16366b2d20fc2e9e722fea36eb5049c1c
Jervx <louellagracechua@gmail.com>
2024-10-06 00:02:44 +0800

    chore: Enhanced Links logic in landing page navbar

--- client/src/components/navbars/auth-nav/index.tsx
+++ client/src/components/navbars/auth-nav/index.tsx
@@ -1,6 +1,6 @@
 import RootNav from '@/components/navbars/root-nav'
 import { ThemeToggleMenu } from '@/components/theme-toggle'
-import NavAuthContents from '@/components/navbars/nav-auth-contents'
+import NavAuthContents from '@/components/navbars/auth-nav/nav-auth-contents'
 
 import { cn } from '@/lib/utils'
 import { IBaseCompNoChild } from '@/types/component/base'
