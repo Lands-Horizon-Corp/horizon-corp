54d6dd8657dd43ef84f1ec8a1ea51978199f769f
Zalven <zalven.dayao@go.team>
2024-10-06 15:17:25 +0800

    feat: adding cookie conset

---
+++ client/src/components/providers/cookie-consent-provider.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import CookieConsent from 'react-cookie-consent'
+
+function App() {
+    const handleAccept = () => {
+        localStorage.setItem('cookieConsent', 'true')
+        document.cookie =
+            'your_cookie_name=your_cookie_value; SameSite=None; Secure; path=/; domain=yourdomain.com'
+    }
+
+    useEffect(() => {
+        // Check if consent has already been given
+        const consent = localStorage.getItem('cookieConsent')
+        if (consent) {
+            document.cookie =
+                'your_cookie_name=your_cookie_value; SameSite=None; Secure; path=/; domain=yourdomain.com'
+        }
+    }, [])
+
+    return (
+        <CookieConsent
+            location="bottom"
+            buttonText="I understand"
+            cookieName="userCookieConsent"
+            style={{ background: '#2B373B' }}
+            buttonStyle={{ color: '#4e503b', fontSize: '13px' }}
+            onAccept={handleAccept}
+            expires={150}
+        >
+            This website uses cookies to enhance the user experience.
+        </CookieConsent>
+    )
+}
+
+export default App
--- client/src/modules/root-layout.tsx
+++ client/src/modules/root-layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from '@tanstack/react-router'
 
 import { Toaster } from '@/components/ui/sonner'
 import ConnectionProvider from '@/components/providers/connection-provider'
+import CookieConsentProvider from '@/components/providers/cookie-consent-provider'
 
 const RootLayout = () => {
     return (
         <div>
+            <CookieConsentProvider />
             <Outlet />
             <Toaster richColors />
             <ConnectionProvider />
