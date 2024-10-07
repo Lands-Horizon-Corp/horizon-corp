84283584061c1239d9cf733e4ada1c9da84b777b
Jervx <louellagracechua@gmail.com>
2024-10-07 12:14:55 +0800

    feat: Added cookie consent alert

--- client/src/modules/root-layout.tsx
+++ client/src/modules/root-layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from '@tanstack/react-router'
 
 import { Toaster } from '@/components/ui/sonner'
+import CookieConsent from '@/components/cookie-consent'
 import ConnectionProvider from '@/components/providers/connection-provider'
 
 const RootLayout = () => {
@@ -9,6 +10,7 @@ const RootLayout = () => {
             <Outlet />
             <Toaster richColors />
             <ConnectionProvider />
+            <CookieConsent />
         </div>
     )
 }
