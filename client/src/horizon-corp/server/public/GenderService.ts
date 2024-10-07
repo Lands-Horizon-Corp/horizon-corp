03d4396347d945bf65e711738e8cf7998180e58e
Zalven <zalven.dayao@go.team>
2024-10-07 09:49:42 +0800

    feat: adding gender service

---
+++ client/src/horizon-corp/server/public/GenderService.ts
@@ -0,0 +1,59 @@
+import UseServer from "../../request/server";
+import { AxiosResponse } from 'axios';
+import type { GendersRequest, GendersResource } from '../../types'
+
+
+class GenderService {
+  private server: UseServer;
+
+  constructor() {
+    this.server = new UseServer();
+  }
+
+  /**
+   * Creates a new gender.
+   *
+   * @param {GendersRequest} genderData - The gender data to create.
+   * @returns {Promise<GenderResource>} - The created gender resource.
+   */
+  async create(genderData: GendersRequest): Promise<GendersResource> {
+    const response: AxiosResponse<GendersResource> = await this.server.post('/genders', genderData);
+    return response.data;
+  }
+
+
+  /**
+   * Deletes a gender by ID.
+   *
+   * @param {number} id - The ID of the gender to delete.
+   * @returns {Promise<void>} - A promise that resolves when the gender is deleted.
+   */
+  async delete(id: number): Promise<void> {
+    await this.server.delete(`/genders/${id}`);
+  }
+
+  /**
+   * Updates an existing gender by ID.
+   *
+   * @param {number} id - The ID of the gender to update.
+   * @param {GendersRequest} genderData - The updated gender data.
+   * @returns {Promise<GenderResource>} - The updated gender resource.
+   */
+  async update(id: number, genderData: GendersRequest): Promise<GendersResource> {
+    const response: AxiosResponse<GendersResource> = await this.server.put(`/genders/${id}`, genderData);
+    return response.data;
+  }
+
+  /**
+  * Gets a gender by ID.
+  *
+  * @param {number} id - The ID of the gender to retrieve.
+  * @returns {Promise<GenderResource>} - The retrieved gender resource.
+  */
+  async getById(id: number): Promise<GendersResource> {
+    const response: AxiosResponse<GendersResource> = await this.server.get(`/genders/${id}`);
+    return response.data;
+  }
+}
+
+export default GenderService
--- client/src/horizon-corp/types/index.ts
+++ client/src/horizon-corp/types/index.ts
@@ -1,4 +1,4 @@
 export * from './contact'
 export * from './roles'
 export * from './error-details'
-export * from './contact'
+export * from './gender'
