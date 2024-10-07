afba62cf902d4c304c16ec662fe7c7cd8132c07e
Zalven <zalven.dayao@go.team>
2024-10-07 21:51:31 +0800

    feat: authentication

---
+++ client/src/horizon-corp/server/auth/UserService.ts
@@ -0,0 +1,28 @@
+/**
+ * Service class to handle CRUD operations for contacts.
+ */
+export default class UserService {
+  private static readonly BASE_ENDPOINT = '/auth'
+
+  public static async signIn() { }
+  public static async signOut() { }
+  public static async signUp() { }
+  public static async changePassword() { }
+  public static async verifyPhone() { }
+  public static async verifyEmail() { }
+  public static async forgotPassword() { }
+  public static async timeIn() { }
+  public static async timeOut() { }
+}
+// sign in
+// sign out
+// sign up
+// change password
+
+// verify phone -> OTP
+// verify email -> EMAIL
+
+// forgot password -> send email
+
+// time in 
+// tim out
