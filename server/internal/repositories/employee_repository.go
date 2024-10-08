5f84e0ed550ad69b27e44744b47c6b3dbde33622
Zalven <zalven.dayao@go.team>
2024-10-08 10:32:56 +0800

    feat: adding users repository

---
+++ server/internal/repositories/employee_repository.go
@@ -0,0 +1,39 @@
+package repositories
+
+import (
+	"horizon/server/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type EmployeeRepository struct {
+	DB *gorm.DB
+}
+
+func NewEmployeeRepository(db *gorm.DB) *EmployeeRepository {
+	return &EmployeeRepository{DB: db}
+}
+
+func (r *EmployeeRepository) Create(employee *models.Employee) error {
+	return r.DB.Create(employee).Error
+}
+
+func (r *EmployeeRepository) GetAll() ([]models.Employee, error) {
+	var employee []models.Employee
+	err := r.DB.Find(&employee).Error
+	return employee, err
+}
+
+func (r *EmployeeRepository) GetByID(id uint) (models.Employee, error) {
+	var employee models.Employee
+	err := r.DB.First(&employee, id).Error
+	return employee, err
+}
+
+func (r *EmployeeRepository) Update(employee *models.Employee) error {
+	return r.DB.Save(employee).Error
+}
+
+func (r *EmployeeRepository) Delete(id uint) error {
+	return r.DB.Delete(&models.Employee{}, id).Error
+}
