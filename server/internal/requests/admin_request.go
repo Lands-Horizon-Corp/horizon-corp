5a49b08c4e9c881d423746552a4d9b2b0952eac6
Zalven <zalven.dayao@go.team>
2024-10-08 10:56:51 +0800

    feat: contact request

---
+++ server/internal/requests/admin_request.go
@@ -0,0 +1,22 @@
+package requests
+
+import "github.com/go-playground/validator/v10"
+
+type AdminRequest struct {
+	FirstName         string       `json:"firstName" validate:"required,max=255"`
+	LastName          string       `json:"lastName" validate:"required,max=255"`
+	PermanentAddress  string       `json:"permanentAddress" validate:"omitempty,max=500"`
+	Description       string       `json:"description" validate:"omitempty,max=1000"`
+	Birthdate         string       `json:"birthdate" validate:"required"`
+	Username          string       `json:"username" validate:"required,max=255"`
+	Email             string       `json:"email" validate:"required,email,max=255"`
+	Password          string       `json:"password" validate:"required,min=8"`
+	Media             MediaRequest `json:"media" validate:"required"`
+	IsEmailVerified   bool         `json:"isEmailVerified" validate:"omitempty"`
+	IsContactVerified bool         `json:"isContactVerified" validate:"omitempty"`
+}
+
+func (r *AdminRequest) Validate() error {
+	validate := validator.New()
+	return validate.Struct(r)
+}
