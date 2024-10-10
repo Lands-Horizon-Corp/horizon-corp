23ed4a3cad64a6753e6d3c44b6fe9eeebb183277
Zalven <zalven.dayao@go.team>
2024-10-10 11:35:15 +0800

    fix: auth request

--- server/internal/requests/authentication/sent_otp_verification_request.go
+++ server/internal/requests/authentication/sent_otp_verification_request.go
@@ -1,1 +1,11 @@
 package requests
+
+import "github.com/go-playground/validator/v10"
+
+type SendOTPVerificationRequest struct {
+}
+
+func (r *SendOTPVerificationRequest) Validate() error {
+	validate := validator.New()
+	return validate.Struct(r)
+}
--- server/internal/requests/authentication/signin_request.go
+++ server/internal/requests/authentication/signin_request.go
@@ -1,1 +1,11 @@
 package requests
+
+import "github.com/go-playground/validator/v10"
+
+type SignInRequest struct {
+}
+
+func (r *SignInRequest) Validate() error {
+	validate := validator.New()
+	return validate.Struct(r)
+}
