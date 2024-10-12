c377bb47e7a013a408556445d71f7136a50255c9
Jervx <louellagracechua@gmail.com>
2024-10-12 09:22:14 +0800

    chore: Add username on auth request resource

--- client/src/horizon-corp/types/auth/request.ts
+++ client/src/horizon-corp/types/auth/request.ts
@@ -3,63 +3,64 @@ import { MediaRequest } from '..'
 export type AccountType = 'Member' | 'Employee' | 'Admin' | 'Owner'
 
 export interface ChangePasswordRequest {
-  currentPassword?: string
-  newPassword: string
-  confirmPassword: string
-  token?: string
+    currentPassword?: string
+    newPassword: string
+    confirmPassword: string
+    token?: string
 }
 
 export interface ForgotPasswordRequest {
-  email: string
-  accountType: AccountType
+    email: string
+    accountType: AccountType
 }
 
 export interface SendEmailVerificationRequest {
-  email: string
+    email: string
 }
 
 export interface SendOTPVerificationRequest {
-  contactNumber: string
+    contactNumber: string
 }
 
 export interface SignInRequest {
-  email: string
-  password: string
+    email: string
+    password: string
 }
 
 export interface SignUpRequest {
-  accountType: AccountType
-  firstName: string
-  lastName: string
-  middleName?: string
-  email: string
-  password: string
-  confirmPassword: string
-  birthdate: Date
-  contactNumber: string
-  permanentAddress: string
-  media?: MediaRequest
+    accountType: AccountType
+    username: string
+    firstName: string
+    lastName: string
+    middleName?: string
+    email: string
+    password: string
+    confirmPassword: string
+    birthdate: Date
+    contactNumber: string
+    permanentAddress: string
+    media?: MediaRequest
 }
 
 export interface VerifyEmailRequest {
-  email: string
-  code: string // 6 digit string
+    email: string
+    code: string // 6 digit string
 }
 
 export interface VerifyOTPRequest {
-  contactNumber: string
-  code: string
+    contactNumber: string
+    code: string
 }
 
 export interface ChangeEmailRequest {
-  email: string
+    email: string
 }
 
 export interface ChangeContactNumberRequest {
-  contactNumber: string
+    contactNumber: string
 }
 
 export interface VerifyContactNumberRequest {
-  contactNumber: string
-  code: string
+    contactNumber: string
+    code: string
 }
