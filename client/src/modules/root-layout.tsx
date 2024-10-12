138732de378c84df51235376a789a6c13ec54487
Jervx <louellagracechua@gmail.com>
2024-10-12 14:32:06 +0800

    feat: Lazy load route pages

--- client/src/modules/root-layout.tsx
+++ client/src/modules/root-layout.tsx
@@ -2,14 +2,12 @@ import { Outlet } from '@tanstack/react-router'
 
 import { Toaster } from '@/components/ui/sonner'
 import CookieConsent from '@/components/cookie-consent'
-import { VersionAndFeedBack } from '@/components/version'
 import ConnectionProvider from '@/providers/connection-provider'
 import ConfirmModal from '@/components/modals/confirm-modal'
 
 const RootLayout = () => {
     return (
         <div className="relative">
-            <VersionAndFeedBack />
             <Outlet />
             <Toaster richColors />
             <ConnectionProvider />
