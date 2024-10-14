71441f6fcd3ec0a999f37f153b2abc809f8d45fc
Jervx <louellagracechua@gmail.com>
2024-10-14 18:43:09 +0800

    feat: Improve sign-in flow

--- client/src/modules/auth/pages/sign-in.tsx
+++ client/src/modules/auth/pages/sign-in.tsx
@@ -9,6 +9,8 @@ import AccountCancelled from '../components/account-cancelled'
 import { UserData } from '@/horizon-corp/types'
 import { emailSchema, userAccountTypeSchema } from '../validations'
 import useCurrentUser from '@/hooks/use-current-user'
+import { getUsersAccountTypeRedirectPage } from './helpers'
+import { toast } from 'sonner'
 
 export const SignInPageSearchSchema = z.object({
     email: z.string().optional().default('').or(emailSchema),
@@ -26,30 +28,30 @@ const SignInPage = () => {
     const prefilledValues = useSearch({ from: '/auth/sign-in' })
     const [userData, setUserData] = useState<null | UserData>(null)
 
-
     const onSignInSuccess = (userData: UserData) => {
-        setCurrentUser(userData)
         const { isContactVerified, isEmailVerified, status } = userData
-        if (status === 'Not Allowed') return setUserData(userData)
+        setCurrentUser(userData)
 
-        if (status === 'Pending' && (!isContactVerified || !isEmailVerified)) {
+        if (status === 'Not Allowed') {
+            toast.error(
+                'Your account has been canceled, and you can no longer log in.'
+            )
+            router.navigate({ to: '/auth/verify' })
+        }
+
+        if (status === 'Pending' || !isContactVerified || !isEmailVerified) {
+            toast.warning('Your account is pending approval')
             router.navigate({ to: '/auth/verify' })
         }
 
         if (status === 'Verified') {
-            // TODO
-            // Navigate to respective page
-            // owner
-            // member
-            // admin
-            // employee
+            const redirectUrl = getUsersAccountTypeRedirectPage(userData)
+            toast.success("You're logged in!")
+            router.navigate({ to: redirectUrl })
         }
     }
 
     const handleOnCancelBack = () => {
-        // TODO:
-        // Sign Out user since he/she is restricted
-        // unset userData
         setUserData(null)
     }
 
