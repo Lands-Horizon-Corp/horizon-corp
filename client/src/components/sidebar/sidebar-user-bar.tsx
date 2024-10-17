c5cc72653f8a555ece48f2881802cea98b1a3296
Jervx <louellagracechua@gmail.com>
2024-10-18 00:10:47 +0800

    feat: Add sign out functionality on sidebar

--- client/src/components/sidebar/sidebar-user-bar.tsx
+++ client/src/components/sidebar/sidebar-user-bar.tsx
@@ -20,16 +20,38 @@ import {
 } from '@/components/icons'
 import UserAvatar from '@/components/user-avatar'
 
-import { cn } from '@/lib/utils'
+import { cn, withCatchAsync } from '@/lib/utils'
 import { useTheme } from '@/providers/theme-provider'
 import { IBaseCompNoChild } from '@/types/component'
+import useCurrentUser from '@/hooks/use-current-user'
+import UserService from '@/horizon-corp/server/auth/UserService'
+import { serverRequestErrExtractor } from '@/helpers'
+import { toast } from 'sonner'
+import { useRouter } from '@tanstack/react-router'
 
 interface Props extends IBaseCompNoChild {
     isExpanded: boolean
 }
 
 const SidebarUserBar = ({ className, isExpanded }: Props) => {
+    const router = useRouter()
     const { setTheme, resolvedTheme } = useTheme()
+    const { currentUser, setCurrentUser } = useCurrentUser({
+        loadOnMount: true,
+    })
+
+    const handleSignout = async () => {
+        const [error] = await withCatchAsync(UserService.SignOut())
+
+        if (error) {
+            const errorMessage = serverRequestErrExtractor({ error })
+            toast.error(errorMessage)
+            return
+        }
+
+        setCurrentUser(null)
+        router.navigate({ to : "/auth"})
+    }
 
     return (
         <DropdownMenu>
@@ -44,14 +66,14 @@ const SidebarUserBar = ({ className, isExpanded }: Props) => {
                 >
                     <span className="inline-flex items-center gap-x-2">
                         <UserAvatar
-                            src="https://avatars.githubusercontent.com/u/48374007?s=80&v=4"
-                            fallback="A"
+                            src={currentUser?.media?.downloadURL ?? ''}
+                            fallback={currentUser?.username?.charAt(0) ?? '-'}
                             className={cn(
                                 'size-9 rounded-[4rem] duration-150 ease-in-out',
                                 !isExpanded && 'hover:rounded-[6px]'
                             )}
                         />
-                        {isExpanded && <p>Amada</p>}
+                        {isExpanded && <p>{currentUser?.username}</p>}
                     </span>
                     {isExpanded && (
                         <ChevronRightIcon className="opacity-30 delay-200 duration-150 ease-in group-hover:opacity-100" />
@@ -101,7 +123,7 @@ const SidebarUserBar = ({ className, isExpanded }: Props) => {
                         <DevIcon className="mr-2 size-4 duration-150 ease-in-out" />
                         <span>Dev Mode</span>
                     </DropdownMenuItem>
-                    <DropdownMenuItem>
+                    <DropdownMenuItem onClick={handleSignout}>
                         <LogoutIcon className="mr-2 size-4 duration-150 ease-in-out" />
                         <span>Sign Out</span>
                     </DropdownMenuItem>
