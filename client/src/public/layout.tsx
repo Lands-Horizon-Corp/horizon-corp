b8715b06c9489e9c5006e62500dc701a4001d8ba
rojan <dctrrohan@gmail.com>
2024-10-21 21:11:48 +0800

    chore: remove unnecessary TextEditor Component

--- client/src/public/layout.tsx
+++ client/src/public/layout.tsx
@@ -1,14 +1,7 @@
-import TextEditor from '@/components/text-editor'
-
 const PublicPracticeLayout = () => {
     return (
         <>
-            <div className="m-auto mt-10 h-[100vh] w-[90%]">
-                <TextEditor
-                    spellCheck={true}
-                    content={`<p>start typing...</p>`}
-                />
-            </div>
+            <div className="m-auto mt-10 h-[100vh] w-[90%]">Testing Route</div>
         </>
     )
 }
