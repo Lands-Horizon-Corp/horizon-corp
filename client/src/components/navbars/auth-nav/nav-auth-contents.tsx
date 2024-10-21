3f4b9bbe012bdbf1d38bb5d1dc3b8362a87a2836
Jervx <louellagracechua@gmail.com>
2024-10-21 18:53:04 +0800

    feat: Add signout on nav

--- client/src/components/navbars/auth-nav/nav-auth-contents.tsx
+++ client/src/components/navbars/auth-nav/nav-auth-contents.tsx
@@ -1,38 +1,75 @@
+import { toast } from 'sonner'
 import { Link, useLocation } from '@tanstack/react-router'
 
 import { Button } from '@/components/ui/button'
 
-import { cn } from '@/lib/utils'
+import { cn, withCatchAsync } from '@/lib/utils'
 import { IBaseCompNoChild } from '@/types/component'
+import useCurrentUser from '@/hooks/use-current-user'
+import { serverRequestErrExtractor } from '@/helpers'
+import UserService from '@/horizon-corp/server/auth/UserService'
+import { useMutation } from '@tanstack/react-query'
 
-interface Props extends IBaseCompNoChild {}
-
-const NavAuthContents = ({ className }: Props) => {
+const NavAuthContents = ({ className }: IBaseCompNoChild) => {
     const pathname = useLocation({
         select: (location) => location.pathname,
+    })
+
+    const { data: currentUser, setCurrentUser } = useCurrentUser({
+        loadOnMount: true,
+    })
+
+    const { mutate: handleSignout, isPending: isSigningOut } = useMutation({
+        mutationKey: ['sign-out'],
+        mutationFn: async () => {
+            const [error] = await withCatchAsync(UserService.SignOut())
+
+            if (error) {
+                const errorMessage = serverRequestErrExtractor({ error })
+                toast.error(errorMessage)
+                return
+            }
+
+            setCurrentUser(null)
+            toast.success('Signed out')
+        },
     })
 
     const shouldShowAuthButtons = !pathname.startsWith('/auth/verify')
 
     return (
         <div className={cn('flex items-center gap-x-2', className)}>
-            {shouldShowAuthButtons && pathname !== '/auth/sign-in' && (
-                <Link to="/auth/sign-in">
-                    <Button className="scale-effects rounded-full bg-green-500 text-white hover:bg-green-500">
-                        Sign-In
-                    </Button>
-                </Link>
+            {currentUser && (
+                <Button
+                    variant="outline"
+                    disabled={isSigningOut}
+                    onClick={() => handleSignout()}
+                    className="scale-effects rounded-full"
+                >
+                    Sign-Out
+                </Button>
             )}
-            {shouldShowAuthButtons && pathname !== '/auth/sign-up' && (
-                <Link to="/auth/sign-up">
-                    <Button
-                        variant="outline"
-                        className="scale-effects rounded-full"
-                    >
-                        Sign-Up
-                    </Button>
-                </Link>
-            )}
+            {shouldShowAuthButtons &&
+                !currentUser &&
+                pathname !== '/auth/sign-in' && (
+                    <Link to="/auth/sign-in">
+                        <Button className="scale-effects rounded-full bg-green-500 text-white hover:bg-green-500">
+                            Sign-In
+                        </Button>
+                    </Link>
+                )}
+            {shouldShowAuthButtons &&
+                !currentUser &&
+                pathname !== '/auth/sign-up' && (
+                    <Link to="/auth/sign-up">
+                        <Button
+                            variant="outline"
+                            className="scale-effects rounded-full"
+                        >
+                            Sign-Up
+                        </Button>
+                    </Link>
+                )}
             {/* TODO: Once auth service is implemeented, add/show user avatar here */}
         </div>
     )
