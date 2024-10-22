6976644c34ca63ecb7a049b5a128f432993ac0b2
Zalven <zalven.dayao@go.team>
2024-10-22 17:00:18 +0800

    feat: adding contact

--- migration/sql/V4__create_contact.sql
+++ migration/sql/V4__create_contact.sql
@@ -1,13 +1,18 @@
 CREATE TABLE IF NOT EXISTS `contacts` (
-    `id` SERIAL PRIMARY KEY,
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(255) NOT NULL,
     `last_name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) UNIQUE NOT NULL,
     `contact_number` VARCHAR(15) NOT NULL,
     description TEXT,
+
+
+    PRIMARY KEY (`id`),
+    `deleted_at` DATETIME(3) DEFAULT NULL,
+     INDEX `idx_contact_deleted_at` (`deleted_at`),
     `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `contacts` (`first_name`, `last_name`, `email`, `contact_number`, `description`)
 VALUES
