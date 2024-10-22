491351e99fea13db4c48876cc49a073f68fee0b4
Zalven <zalven.dayao@go.team>
2024-10-22 16:14:27 +0800

    feat: adding description

--- client/src/horizon-corp/server/auth/ProfileService.ts
+++ client/src/horizon-corp/server/auth/ProfileService.ts
@@ -1,18 +1,28 @@
 import { AxiosResponse } from 'axios'
 import UseServer from '@/horizon-corp/request/server'
-import { CurrentUserResource, MediaRequest } from '@/horizon-corp/types'
+import { CurrentUserResource, DescriptionRequest, MediaRequest } from '@/horizon-corp/types'
 
 export default class ProfileService {
-    private static readonly BASE_ENDPOINT = '/profile'
+  private static readonly BASE_ENDPOINT = '/profile'
 
-    // POST - /profile/profile-picture
-    public static async ProfilePicture(
-        data: MediaRequest
-    ): Promise<AxiosResponse<CurrentUserResource>> {
-        const endpoint = `${ProfileService.BASE_ENDPOINT}/profile-picture`
-        return await UseServer.post<MediaRequest, CurrentUserResource>(
-            endpoint,
-            data
-        )
-    }
+  // POST - /profile/profile-picture
+  public static async ProfilePicture(
+    data: MediaRequest
+  ): Promise<AxiosResponse<CurrentUserResource>> {
+    const endpoint = `${ProfileService.BASE_ENDPOINT}/profile-picture`
+    return await UseServer.post<MediaRequest, CurrentUserResource>(
+      endpoint,
+      data
+    )
+  }
+
+  public static async Description(
+    data: DescriptionRequest
+  ): Promise<AxiosResponse<CurrentUserResource>> {
+    const endpoint = `${ProfileService.BASE_ENDPOINT}/description`
+    return await UseServer.post<DescriptionRequest, CurrentUserResource>(
+      endpoint,
+      data
+    )
+  }
 }
--- client/src/horizon-corp/types/auth/request.ts
+++ client/src/horizon-corp/types/auth/request.ts
@@ -3,72 +3,76 @@ import { MediaRequest } from '..'
 export type AccountType = 'Member' | 'Employee' | 'Admin' | 'Owner'
 
 export interface ChangePasswordRequest {
-    otp?: string
-    resetId?: string
-    newPassword: string
-    confirmPassword: string
+  otp?: string
+  resetId?: string
+  newPassword: string
+  confirmPassword: string
 }
 
 export interface ForgotPasswordRequest {
-    key: string
-    accountType: string
-    emailTemplate?: string
-    contactTemplate?: string
+  key: string
+  accountType: string
+  emailTemplate?: string
+  contactTemplate?: string
 }
 
 export interface SendEmailVerificationRequest {
-    emailTemplate?: string
+  emailTemplate?: string
 }
 
 export interface SendContactNumberVerificationRequest {
-    contactTemplate?: string
+  contactTemplate?: string
 }
 
 export interface SignInRequest {
-    email?: string
-    username?: string
-    password: string
-    accountType: AccountType
+  email?: string
+  username?: string
+  password: string
+  accountType: AccountType
 }
 
 export interface SignUpRequest {
-    accountType: AccountType
-    username: string
-    firstName: string
-    lastName: string
-    middleName?: string
-    email: string
-    password: string
-    confirmPassword: string
-    birthdate: Date
-    contactNumber: string
-    permanentAddress: string
-    media?: MediaRequest
+  accountType: AccountType
+  username: string
+  firstName: string
+  lastName: string
+  middleName?: string
+  email: string
+  password: string
+  confirmPassword: string
+  birthdate: Date
+  contactNumber: string
+  permanentAddress: string
+  media?: MediaRequest
 
-    emailTemplate?: string
-    contactTemplate?: string
+  emailTemplate?: string
+  contactTemplate?: string
 }
 
 export interface VerifyEmailRequest {
-    otp: string
+  otp: string
 }
 
 export interface VerifyContactNumberRequest {
-    otp: string
+  otp: string
 }
 
 export interface ChangeEmailRequest {
-    email: string
-    emailTemplate?: string
+  email: string
+  emailTemplate?: string
 }
 
 export interface ChangeContactNumberRequest {
-    contactNumber: string
-    contactTemplate?: string
+  contactNumber: string
+  contactTemplate?: string
 }
 
 export interface NewPasswordRequest {
-    PreviousPassword: string
-    NewPassword: string
-    ConfirmPassword: string
+  PreviousPassword: string
+  NewPassword: string
+  ConfirmPassword: string
+}
+
+export interface DescriptionRequest {
+  description: string
 }
