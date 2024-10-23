5f14970c3045a103f89e2c4de0d126a62063bade
Zalven <zalven.dayao@go.team>
2024-10-23 18:06:56 +0800

    feat: fix icon and merge

--- client/src/components/icons.tsx
+++ client/src/components/icons.tsx
@@ -52,16 +52,23 @@ import {
     AiOutlineKey as KeyIcon,
     AiOutlineLoading as LoadingCircleIcon,
     AiOutlineLoading3Quarters as LoadingSpinnerIcon,
+    AiOutlineMessage as AiOutlineMessageIcon,
 } from 'react-icons/ai'
 import { GoArrowLeft as ArrowLeftIcon } from 'react-icons/go'
-import { MdMarkEmailRead as EmailCheckIcon } from 'react-icons/md'
+import {
+    MdMarkEmailRead as EmailCheckIcon,
+    MdOutlineEmail as MdOutlineEmailIcon,
+} from 'react-icons/md'
 import {
     BsPatchCheck as VerifiedPatchIcon,
     BsFillPatchMinusFill as PatchMinusIcon,
     BsFillPatchCheckFill as PatchCheckIcon,
     BsPatchExclamationFill as PatchExclamationIcon,
 } from 'react-icons/bs'
-import { IoFootstepsOutline as FootstepsIcon } from 'react-icons/io5'
+import {
+    IoFootstepsOutline as FootstepsIcon,
+    IoCallOutline as IoCallOutlineIcon,
+} from 'react-icons/io5'
 import { RxFontItalic as ToolbarItalicIcon } from 'react-icons/rx'
 import {
     FaBold as FaBoldIcon,
@@ -200,4 +207,7 @@ export {
     CgFacebookIcon,
     PiTextStrikethroughLightIcon,
     PiListBulletsBoldIcon,
+    AiOutlineMessageIcon,
+    MdOutlineEmailIcon,
+    IoCallOutlineIcon,
 }
