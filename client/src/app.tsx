8b63bf04b149d31d0f5812bf718404d42fb1593b
Zalven <zalven.dayao@go.team>
2024-10-23 17:55:20 +0800

    adding environment

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,10 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouterProvider } from '@tanstack/react-router';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import router from '@/root-route';
 import { ThemeProvider } from '@/providers/theme-provider';
+import { EnvironmentManager } from './manager';
 
 const TanStackRouterDevtoolsPanel =
     process.env.NODE_ENV === 'production'
@@ -23,7 +24,10 @@ declare module '@tanstack/react-router' {
 
 const App = () => {
     const [queryClient] = useState(new QueryClient());
-
+    useEffect(() => {
+        const initializeEnv = async () => await EnvironmentManager.initialize();
+        initializeEnv();
+    }, []);
     return (
         <QueryClientProvider client={queryClient}>
             <ThemeProvider>
