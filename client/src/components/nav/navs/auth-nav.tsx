22262fe77103b298110211bdb5fb1e5be73498b0
Jervx <louellagracechua@gmail.com>
2024-10-25 16:59:44 +0800

    chore: Format codebase

--- client/src/components/nav/navs/auth-nav.tsx
+++ client/src/components/nav/navs/auth-nav.tsx
@@ -5,10 +5,9 @@ import NavAuthGroup from '@/components/nav/nav-components/nav-auth-group'
 import NavThemeToggle from '@/components/nav/nav-components/nav-theme-toggle'
 
 const AuthNav = () => {
-
     return (
         <RootNav className="pointer-events-none fixed w-full">
-            <NavEcoopLogo className='pointer-events-auto' />
+            <NavEcoopLogo className="pointer-events-auto" />
             <NavContainer></NavContainer>
             <NavContainer className="pointer-events-auto">
                 <NavAuthGroup />
