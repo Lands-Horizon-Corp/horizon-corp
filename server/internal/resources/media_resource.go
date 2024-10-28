872828c6a943a26ccf4a5b604186496781f418c0
Zalven <zalven.dayao@go.team>
2024-10-28 11:45:02 +0800

    fix: media download url

--- server/internal/resources/media_resource.go
+++ server/internal/resources/media_resource.go
@@ -7,20 +7,21 @@ import (
 )
 
 type MediaResource struct {
-	ID         uint                `json:"id"`
-	FileName   string              `json:"fileName"`
-	FileSize   int64               `json:"fileSize"`
-	FileType   string              `json:"fileType"`
-	StorageKey string              `json:"storageKey"`
-	URL        string              `json:"url"`
-	Key        string              `json:"key,omitempty"`
-	BucketName string              `json:"bucketName,omitempty"`
-	Employees  []*EmployeeResource `json:"employees,omitempty"` // Updated to slice of pointers
-	Members    []*MemberResource   `json:"members,omitempty"`   // Updated to slice of pointers
-	Owners     []*OwnerResource    `json:"owners,omitempty"`    // Updated to slice of pointers
-	Admins     []*AdminResource    `json:"admins,omitempty"`    // Updated to slice of pointers
-	Companies  []*CompanyResource  `json:"companies,omitempty"` // Updated to slice of pointers
-	Branches   []*BranchResource   `json:"branches,omitempty"`  // Updated to slice of pointers
+	ID          uint                `json:"id"`
+	FileName    string              `json:"fileName"`
+	FileSize    int64               `json:"fileSize"`
+	FileType    string              `json:"fileType"`
+	StorageKey  string              `json:"storageKey"`
+	URL         string              `json:"url"`
+	Key         string              `json:"key,omitempty"`
+	DownloadURL string              `json:"downloadURL"`
+	BucketName  string              `json:"bucketName,omitempty"`
+	Employees   []*EmployeeResource `json:"employees,omitempty"` // Updated to slice of pointers
+	Members     []*MemberResource   `json:"members,omitempty"`   // Updated to slice of pointers
+	Owners      []*OwnerResource    `json:"owners,omitempty"`    // Updated to slice of pointers
+	Admins      []*AdminResource    `json:"admins,omitempty"`    // Updated to slice of pointers
+	Companies   []*CompanyResource  `json:"companies,omitempty"` // Updated to slice of pointers
+	Branches    []*BranchResource   `json:"branches,omitempty"`  // Updated to slice of pointers
 
 	CreatedAt string `json:"createdAt"`
 	UpdatedAt string `json:"updatedAt"`
@@ -35,24 +36,28 @@ func ToResourceMedia(media *models.Media) *MediaResource {
 	if media == nil {
 		return nil
 	}
+	if media == nil {
+		return nil
+	}
 
 	return &MediaResource{
-		ID:         media.ID,
-		FileName:   media.FileName,
-		FileSize:   media.FileSize,
-		FileType:   media.FileType,
-		StorageKey: media.StorageKey,
-		URL:        temporaryURL,
-		Key:        media.Key,
-		BucketName: media.BucketName,
-		Employees:  ToResourceListEmployees(media.Employees),
-		Members:    ToResourceListMembers(media.Members),
-		Owners:     ToResourceListOwners(media.Owners),
-		Admins:     ToResourceListAdmins(media.Admins),
-		Companies:  ToResourceListCompanies(media.Companies),
-		Branches:   ToResourceListBranch(media.Branches),
-		CreatedAt:  media.CreatedAt.Format(time.RFC3339),
-		UpdatedAt:  media.UpdatedAt.Format(time.RFC3339),
+		ID:          media.ID,
+		FileName:    media.FileName,
+		FileSize:    media.FileSize,
+		FileType:    media.FileType,
+		StorageKey:  media.StorageKey,
+		URL:         media.URL,
+		Key:         media.Key,
+		BucketName:  media.BucketName,
+		DownloadURL: temporaryURL,
+		Employees:   ToResourceListEmployees(media.Employees),
+		Members:     ToResourceListMembers(media.Members),
+		Owners:      ToResourceListOwners(media.Owners),
+		Admins:      ToResourceListAdmins(media.Admins),
+		Companies:   ToResourceListCompanies(media.Companies),
+		Branches:    ToResourceListBranch(media.Branches),
+		CreatedAt:   media.CreatedAt.Format(time.RFC3339),
+		UpdatedAt:   media.UpdatedAt.Format(time.RFC3339),
 	}
 }
 
