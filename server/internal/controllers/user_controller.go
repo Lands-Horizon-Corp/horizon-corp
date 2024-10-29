f5fd3aa121a4c0b46eaffcc6175a8c0cf6c97570
Zalven <zalven.dayao@go.team>
2024-10-29 12:32:26 +0800

    feat: adding change password and account settings

--- server/internal/controllers/user_controller.go
+++ server/internal/controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"horizon/server/config"
 	"horizon/server/internal/auth"
 	"horizon/server/internal/middleware"
 	"horizon/server/internal/models"
@@ -67,8 +68,8 @@ func (c *UserController) ProfilePicture(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, response)
 }
 
-func (c *UserController) Description(ctx *gin.Context) {
-	var req user_requests.DescriptionRequest
+func (c *UserController) AccountSetting(ctx *gin.Context) {
+	var req user_requests.AccountSettingRequest
 
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -93,8 +94,12 @@ func (c *UserController) Description(ctx *gin.Context) {
 		return
 	}
 	user := &models.User{
-		AccountType: userClaims.AccountType,
-		Description: req.Description,
+		AccountType:      userClaims.AccountType,
+		Birthdate:        req.Birthdate,
+		FirstName:        req.FirstName,
+		LastName:         req.LastName,
+		Description:      req.Description,
+		PermanentAddress: req.PermanentAddress,
 	}
 	updatedUser, err := c.userRepo.UpdateColumns(userClaims.ID, user)
 	if err != nil {
@@ -107,11 +112,53 @@ func (c *UserController) Description(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, response)
 }
 
+func (c *UserController) ChangePasswordSetting(ctx *gin.Context) {
+	var req user_requests.ChangePasswordSettingRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if err := req.Validate(); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	claims, exists := ctx.Get("claims")
+	if !exists {
+		c.tokenService.ClearTokenCookie(ctx)
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated."})
+		return
+	}
+	userClaims, ok := claims.(*auth.UserClaims)
+	if !ok {
+		c.tokenService.ClearTokenCookie(ctx)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve user claims."})
+		return
+	}
+
+	user, err := c.userRepo.GetByID(userClaims.AccountType, userClaims.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "User not found."})
+		return
+	}
+
+	if !config.VerifyPassword(user.Password, req.ConfirmPassword) {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Password verification failed."})
+		return
+	}
+	if err := c.userRepo.UpdatePassword(userClaims.AccountType, userClaims.ID, req.ConfirmPassword); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update password."})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Password changed successfully."})
+}
+
 func UserRoutes(router *gin.RouterGroup, mw *middleware.AuthMiddleware, controller *UserController) {
 	authGroup := router.Group("/profile")
 	{
 		authGroup.Use(mw.Middleware())
 		authGroup.POST("/profile-picture", controller.ProfilePicture)
-		authGroup.POST("/description", controller.Description)
+		authGroup.POST("/account-setting", controller.AccountSetting)
+		authGroup.POST("/change-password", controller.ChangePasswordSetting)
 	}
 }
