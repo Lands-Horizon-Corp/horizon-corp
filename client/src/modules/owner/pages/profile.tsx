7a3418e14323982bc54995a207ef0102b3fa6818
Jervx <louellagracechua@gmail.com>
2024-11-04 11:00:46 +0800

    style: Adjusted padding on mobile

--- client/src/modules/owner/pages/profile.tsx
+++ client/src/modules/owner/pages/profile.tsx
@@ -2,7 +2,7 @@ import AccountProfile from '@/components/account-profile'
 
 const OwnerProfilePage = () => {
     return (
-        <div className="flex w-full max-w-full flex-col items-center px-2 sm:px-8">
+        <div className="flex w-full max-w-full flex-col items-center px-4 sm:px-8">
             <AccountProfile />
         </div>
     )
