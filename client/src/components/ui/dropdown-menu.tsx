98f6d181d5de245d809535048faf9a7a0b4a33f1
Jervx <louellagracechua@gmail.com>
2024-11-05 19:27:22 +0800

    chore: Refactor ui components and icon

--- client/src/components/ui/dropdown-menu.tsx
+++ client/src/components/ui/dropdown-menu.tsx
@@ -102,14 +102,15 @@ const DropdownMenuCheckboxItem = React.forwardRef<
         ref={ref}
         className={cn(
             'relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+            'group cursor-pointer rounded-lg text-foreground/70 focus:bg-primary focus:text-primary-foreground',
             className
         )}
         checked={checked}
         {...props}
     >
-        <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+        <span className="absolute left-2 flex size-4 items-center justify-center">
             <DropdownMenuPrimitive.ItemIndicator>
-                <Check className="h-4 w-4" />
+                <Check className="size-4" />
             </DropdownMenuPrimitive.ItemIndicator>
         </span>
         {children}
