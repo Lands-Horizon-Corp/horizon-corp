1c271df371f7d8b13f856175e4bc4b0c28505e18
Jervx <louellagracechua@gmail.com>
2024-12-16 16:45:16 +0800

    chore: Improve get started message handling when account type is invalid

--- client/src/components/nav/nav-components/nav-get-started.tsx
+++ client/src/components/nav/nav-components/nav-get-started.tsx
@@ -1,12 +1,14 @@
-import { Link } from '@tanstack/react-router'
-
 import { Button } from '@/components/ui/button'
 import UserAvatar from '@/components/user-avatar'
 
 import { getUsersAccountTypeRedirectPage } from '@/helpers'
 import { useUserAuthStore } from '@/store/user-auth-store'
+import { toast } from 'sonner'
+import logger from '@/helpers/loggers/logger'
+import { useRouter } from '@tanstack/react-router'
 
 const NavGetStarted = () => {
+    const { navigate } = useRouter()
     const { currentUser } = useUserAuthStore()
 
     if (!currentUser) return null
@@ -14,16 +16,26 @@ const NavGetStarted = () => {
     const redirectUrl = getUsersAccountTypeRedirectPage(currentUser)
 
     return (
-        <Link to={redirectUrl}>
-            <Button className="scale-effects gap-x-2 rounded-full px-2">
-                <UserAvatar
-                    src={currentUser.media?.downloadURL ?? ''}
-                    fallback={currentUser?.username.charAt(0) ?? '-'}
-                    fallbackClassName="bg-secondary text-secondary-foreground"
-                />
-                <span className="mr-2">Get Started</span>
-            </Button>
-        </Link>
+        <Button
+            onClick={() => {
+                if (redirectUrl === '/') {
+                    toast.error(
+                        "'Get Started' is not available for your account type. Contact your admin for help."
+                    )
+                    return
+                }
+
+                navigate({ to: redirectUrl })
+            }}
+            className="scale-effects gap-x-2 rounded-full px-2"
+        >
+            <UserAvatar
+                src={currentUser.media?.downloadURL ?? ''}
+                fallback={currentUser?.username.charAt(0) ?? '-'}
+                fallbackClassName="bg-secondary text-secondary-foreground"
+            />
+            <span className="mr-2">Get Started</span>
+        </Button>
     )
 }
 
