a2a6b66df2fac0a14d398bf000a8437884e74101
Zalven <zalven.dayao@go.team>
2024-12-23 14:27:16 +0800

    feat: preload

--- engine/internal/modules/company/company_export.go
+++ engine/internal/modules/company/company_export.go
@@ -19,17 +19,9 @@ func NewCompanyExport(db *providers.DatabaseService) *CompanyExport {
 }
 
 func (ce *CompanyExport) ExportAll(c *gin.Context) {
-
-	// Initialize the CSVManager
 	csvManager := managers.NewCSVManager()
-
-	// Set the desired file name
 	csvManager.SetFileName("sample.csv")
-
-	// Define headers
 	csvManager.SetHeaders([]string{"Name", "Age", "Email", "Country"})
-
-	// Add records (could be fetched from a database or other sources)
 	records := [][]string{
 		{"John Doe", "30", "john.doe@example.com", "USA"},
 		{"Jane Smith", "25", "jane.smith@example.com", "Canada"},
@@ -38,7 +30,6 @@ func (ce *CompanyExport) ExportAll(c *gin.Context) {
 		{"Michael Brown", "35", "michael.brown@example.com", "New Zealand"},
 	}
 	csvManager.AddRecords(records)
-
 	if err := csvManager.WriteCSV(c); err != nil {
 		c.String(http.StatusInternalServerError, "Failed to generate CSV: %v", err)
 		return
