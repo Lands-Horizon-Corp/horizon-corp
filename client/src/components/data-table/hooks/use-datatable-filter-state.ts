24d45bb6e645f8ab0fe053e54ba557de891fbb34
Jervx <louellagracechua@gmail.com>
2024-12-27 10:09:29 +0800

    refactor: Refactor final filter payload

--- client/src/components/data-table/hooks/use-datatable-filter-state.ts
+++ client/src/components/data-table/hooks/use-datatable-filter-state.ts
@@ -6,6 +6,7 @@ import {
     TSearchFilter,
     TFilterObject,
     IDataTableFilterState,
+    TFilterPayload,
 } from '../data-table-filters/data-table-filter-context'
 import useDebounce from '@/hooks/use-debounce'
 
@@ -58,66 +59,59 @@ const useDatableFilterState = (options?: {
         options?.debounceFinalFilterMs ?? 800
     )
 
-    const finalFilters: { filters: TFinalFilter[]; logic: TFilterLogic } =
-        useMemo(() => {
-            const filteredFilter: TFinalFilter[] = []
+    const finalFilterPayload: TFilterPayload = useMemo(() => {
+        const filteredFilter: TFinalFilter[] = []
 
-            Object.entries(debouncedFilter).forEach(([key, value]) => {
-                if (!value || !value.value) {
-                    // logger.log('Value failed', value)
-                    return
-                }
+        Object.entries(debouncedFilter).forEach(([key, value]) => {
+            if (!value || !value.value) {
+                // logger.log('Value failed', value)
+                return
+            }
 
-                if (!value.mode || key === 'globalSearch') {
-                    // logger.log( 'value mode, globalSearch failed', value.mode, key)
-                    return
-                }
+            if (!value.mode || key === 'globalSearch') {
+                // logger.log( 'value mode, globalSearch failed', value.mode, key)
+                return
+            }
 
-                if (
-                    value.mode === 'range' &&
-                    !Array.isArray(value.value) &&
-                    (value.from === undefined || value.to === undefined)
-                ) {
-                    // logger.log( 'line 80 failed -> invalid range from to', value.from, value.to)
-                    return
-                } else if (
-                    value.mode !== 'range' &&
-                    value.value === undefined
-                ) {
-                    // logger.log('line 86 failed -> invalid value', value.value)
-                    return
-                } else if (
-                    Array.isArray(value.value) &&
-                    value.value.length === 0
-                ) {
-                    // logger.log( 'line 89 failed -> invalid multi select array', value.value)
-                    return
-                }
+            if (
+                value.mode === 'range' &&
+                !Array.isArray(value.value) &&
+                (value.from === undefined || value.to === undefined)
+            ) {
+                // logger.log( 'line 80 failed -> invalid range from to', value.from, value.to)
+                return
+            } else if (value.mode !== 'range' && value.value === undefined) {
+                // logger.log('line 86 failed -> invalid value', value.value)
+                return
+            } else if (Array.isArray(value.value) && value.value.length === 0) {
+                // logger.log( 'line 89 failed -> invalid multi select array', value.value)
+                return
+            }
 
-                filteredFilter.push({
-                    field: key,
-                    mode: value.mode,
-                    dataType: value.dataType,
-                    isStaticFilter : value.isStaticFilter,
-                    value:
-                        value.mode === 'range' && !Array.isArray(value.value)
-                            ? { from: value.from, to: value.to }
-                            : value.value,
-                })
+            filteredFilter.push({
+                field: key,
+                mode: value.mode,
+                dataType: value.dataType,
+                isStaticFilter: value.isStaticFilter,
+                value:
+                    value.mode === 'range' && !Array.isArray(value.value)
+                        ? { from: value.from, to: value.to }
+                        : value.value,
             })
+        })
 
-            options?.onFilterChange?.()
+        options?.onFilterChange?.()
 
-            return { filters: filteredFilter, logic: filterLogic }
+        return { filters: filteredFilter, logic: filterLogic }
 
-            // WARNING: don't worry about this, if you remove this and follow the suggestion, infinite loop will happen
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, [debouncedFilter, filterLogic])
+        // WARNING: don't worry about this, if you remove this and follow the suggestion, infinite loop will happen
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [debouncedFilter, filterLogic])
 
     return {
         filters,
         filterLogic,
-        finalFilters,
+        finalFilterPayload,
         setFilter,
         resetFilter,
         removeFilter,
