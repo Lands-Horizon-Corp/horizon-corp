b76b6c93c290cb92537e235050ad144003864b1f
Zalven <zalven.dayao@go.team>
2025-01-10 19:29:39 +0800

    feat: preload

---
+++ server/internal/api/controllers/footstep-controller.go
@@ -0,0 +1,9 @@
+package controllers
+
+// GET: /
+// Retrieve only my footsteps.
+
+// GET: /team
+//     If the user is an admin, they can retrieve members, employees, and owners from all branches.
+//     If the user is an owner, they can retrieve all employees and members from their own branches.
+//     If the user is an employee, they can retrieve footsteps of other employees and members from their specific branch.
