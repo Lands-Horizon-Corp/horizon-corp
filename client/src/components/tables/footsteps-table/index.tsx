8ddbc513e1e805a6289c393bd5c21a008b2a9d09
Jervx <louellagracechua@gmail.com>
2025-01-16 12:10:42 +0800

    feat: Add footstep service and footstep table

---
+++ client/src/components/tables/footsteps-table/index.tsx
@@ -0,0 +1,175 @@
+import {
+    useReactTable,
+    getCoreRowModel,
+    getSortedRowModel,
+} from '@tanstack/react-table'
+import { useMemo } from 'react'
+
+import DataTable from '@/components/data-table'
+import DataTableToolbar, {
+    IDataTableToolbarProps,
+} from '@/components/data-table/data-table-toolbar'
+import DataTablePagination from '@/components/data-table/data-table-pagination'
+import useDataTableState from '@/components/data-table/hooks/use-datatable-state'
+
+import footstepColumns, {
+    footstepGlobalSearchTargets,
+    IFootstepTableColumnProps,
+} from './columns'
+
+import { cn } from '@/lib'
+import { TableProps } from '../types'
+import { FootstepResource } from '@/horizon-corp/types'
+import { FootstepService } from '@/horizon-corp/services'
+import useDatableFilterState from '@/hooks/use-filter-state'
+import FilterContext from '@/contexts/filter-context/filter-context'
+import { useFilteredPaginatedFootsteps } from '@/hooks/api-hooks/use-footstep'
+
+export interface FootstepTableProps
+    extends TableProps<FootstepResource>,
+        IFootstepTableColumnProps {
+    toolbarProps?: Omit<
+        IDataTableToolbarProps<FootstepResource>,
+        | 'table'
+        | 'refreshActionProps'
+        | 'globalSearchProps'
+        | 'scrollableProps'
+        | 'filterLogicProps'
+        | 'exportActionProps'
+        | 'deleteActionProps'
+    >
+}
+
+const FootstepTable = ({
+    className,
+    toolbarProps,
+    defaultFilter,
+    onSelectData,
+    actionComponent,
+}: FootstepTableProps) => {
+    const columns = useMemo(
+        () =>
+            footstepColumns({
+                actionComponent,
+            }),
+        [actionComponent]
+    )
+
+    const {
+        sorting,
+        sortingState,
+        setSorting,
+        getRowIdFn,
+        pagination,
+        setPagination,
+        columnOrder,
+        setColumnOrder,
+        isScrollable,
+        setIsScrollable,
+        columnVisibility,
+        setColumnVisibility,
+        rowSelectionState,
+        createHandleRowSelectionChange,
+    } = useDataTableState<FootstepResource>({
+        columnOrder: columns.map((c) => c.id!),
+        onSelectData,
+    })
+
+    const filterState = useDatableFilterState({
+        defaultFilter,
+        onFilterChange: () => setPagination({ ...pagination, pageIndex: 0 }),
+    })
+
+    const {
+        isPending,
+        isRefetching,
+        data: { data, totalPage, pageSize, totalSize },
+        refetch,
+    } = useFilteredPaginatedFootsteps({
+        pagination,
+        sort: sortingState,
+        filterPayload: filterState.finalFilterPayload,
+    })
+
+    const handleRowSelectionChange = createHandleRowSelectionChange(data)
+
+    const table = useReactTable({
+        columns,
+        data: data,
+        initialState: {
+            columnPinning: { left: ['select'] },
+        },
+        state: {
+            sorting,
+            pagination,
+            columnOrder,
+            rowSelection: rowSelectionState.rowSelection,
+            columnVisibility,
+        },
+        rowCount: pageSize,
+        manualSorting: true,
+        pageCount: totalPage,
+        enableMultiSort: false,
+        manualFiltering: true,
+        manualPagination: true,
+        getRowId: getRowIdFn,
+        onSortingChange: setSorting,
+        onPaginationChange: setPagination,
+        getCoreRowModel: getCoreRowModel(),
+        onColumnOrderChange: setColumnOrder,
+        getSortedRowModel: getSortedRowModel(),
+        onColumnVisibilityChange: setColumnVisibility,
+        onRowSelectionChange: handleRowSelectionChange,
+    })
+
+    return (
+        <FilterContext.Provider value={filterState}>
+            <div className={cn('flex h-full flex-col gap-y-2', className)}>
+                <DataTableToolbar
+                    globalSearchProps={{
+                        defaultMode: 'equal',
+                        targets: footstepGlobalSearchTargets,
+                    }}
+                    table={table}
+                    refreshActionProps={{
+                        onClick: () => refetch(),
+                        isLoading: isPending || isRefetching,
+                    }}
+                    scrollableProps={{ isScrollable, setIsScrollable }}
+                    exportActionProps={{
+                        pagination,
+                        isLoading: isPending,
+                        filters: filterState.finalFilterPayload,
+                        disabled: isPending || isRefetching,
+                        exportAll: FootstepService.exportAll,
+                        exportAllFiltered: FootstepService.exportAllFiltered,
+                        exportCurrentPage: (ids) =>
+                            FootstepService.exportSelected(
+                                ids.map((data) => data.id)
+                            ),
+                        exportSelected: (ids) =>
+                            FootstepService.exportSelected(
+                                ids.map((data) => data.id)
+                            ),
+                    }}
+                    filterLogicProps={{
+                        filterLogic: filterState.filterLogic,
+                        setFilterLogic: filterState.setFilterLogic,
+                    }}
+                    {...toolbarProps}
+                />
+                <DataTable
+                    table={table}
+                    isStickyHeader
+                    isStickyFooter
+                    isScrollable={isScrollable}
+                    setColumnOrder={setColumnOrder}
+                    className="mb-2"
+                />
+                <DataTablePagination table={table} totalSize={totalSize} />
+            </div>
+        </FilterContext.Provider>
+    )
+}
+
+export default FootstepTable
