9de07c2501b03a3cc34e4ffb428909c6fe30323f
Jervx <louellagracechua@gmail.com>
2025-01-23 16:13:59 +0800

    feat: Add branch table with static filter

--- client/src/modules/owner/pages/company/branches.tsx
+++ client/src/modules/owner/pages/company/branches.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react'
+
+import BranchesTable from '@/components/tables/branches-table'
 import PageContainer from '@/components/containers/page-container'
+
 import EnsureOwnerCompany from '../../components/ensure-company'
 
+import { ICompanyResource } from '@/server'
+
 const OwnerCompanyBranchesPage = () => {
+    const [company, setCompany] = useState<ICompanyResource | undefined>(
+        undefined
+    )
+
     return (
         <PageContainer>
-            <EnsureOwnerCompany></EnsureOwnerCompany>
+            <EnsureOwnerCompany onSuccess={setCompany}>
+                {company && (
+                    <BranchesTable
+                        toolbarProps={{
+                            createActionProps: {
+                                onClick: () => {},
+                            },
+                        }}
+                        className="min-h-[90vh] w-full"
+                        defaultFilter={{
+                            companyId: {
+                                value: company.id,
+                                mode: 'equal',
+                                displayText: '',
+                                isStaticFilter: true,
+                                dataType: 'number',
+                            },
+                        }}
+                    />
+                )}
+            </EnsureOwnerCompany>
         </PageContainer>
     )
 }
--- client/src/modules/owner/pages/footstep-tracking.tsx
+++ client/src/modules/owner/pages/footstep-tracking.tsx
@@ -1,10 +1,13 @@
+import PageContainer from '@/components/containers/page-container'
 import FootstepTable from '@/components/tables/footsteps-table'
 
 const OwnerFootstepTrackingPage = () => {
     return (
-        <div className="flex w-full max-w-full flex-col items-center px-4 pb-6 sm:px-8">
+        <PageContainer>
+            {/* <div className="flex w-full max-w-full flex-col items-center px-4 pb-6 sm:px-8"> */}
             <FootstepTable mode="team" className="min-h-[90vh] w-full" />
-        </div>
+            {/* </div> */}
+        </PageContainer>
     )
 }
 
