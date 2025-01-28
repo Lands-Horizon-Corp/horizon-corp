0218a1b902fb9a8ee083068acac0d0d311548e1f
Jervx <louellagracechua@gmail.com>
2025-01-28 13:35:19 +0800

    refactor: Refactor server api types

--- client/src/server/types/member.ts
+++ client/src/server/types/member.ts
@@ -1,9 +1,9 @@
-import { IUserBase } from './common'
 import { IRolesResource } from './role'
-import { IMediaRequest } from './media'
-import { IBranchResource } from './branch'
+import { IGenderResource } from './gender'
 import { IFootstepResource } from './footstep'
 import { IPaginatedResult } from './paginated-result'
+import { ITimeStamps, TAccountStatus } from './common'
+import { IMediaRequest, IMediaResource } from './media'
 
 export interface IMemberRequest {
     username: string
@@ -22,15 +22,39 @@ export interface IMemberRequest {
 
     emailTemplate?: string
     contactTemplate?: string
+
+    companyId?: number
 }
 
-export interface IMemberResource extends IUserBase {
-    accountType: string
-    longitude?: number
-    latitude?: number
-    branch?: IBranchResource
-    role?: IRolesResource
-    footsteps?: IFootstepResource[]
+export interface IMemberResource extends ITimeStamps {
+    id: string
+    accountType: 'Member'
+
+    username: string
+    description?: string
+    isEmailVerified: boolean
+    isContactVerified: boolean
+    isSkipVerification: boolean
+
+    email: string
+    contactNumber: string
+    permanentAddress?: string
+    role?: IRolesResource // Optional due to `omitempty`
+    gender?: IGenderResource // Optional due to `omitempty`
+
+    firstName: string
+    lastName: string
+    middleName?: string // Optional due to `omitempty`
+    fullName: string
+    birthDate: Date // `time.Time` maps to `Date` in TypeScript
+
+    status: TAccountStatus // Assuming `providers.UserStatus` maps to a string; adjust if it's an enum
+
+    media?: IMediaResource // Optional due to `omitempty`
+    longitude?: number // `float64` maps to `number` in TypeScript
+    latitude?: number // `float64` maps to `number` in TypeScript
+    footsteps?: IFootstepResource[] // Array of `FootstepResource`
+    // memberProfile?: MemberProfileResource; // Optional due to `omitempty`
 }
 
 export type IMemberPaginatedResource = IPaginatedResult<IMemberResource>
