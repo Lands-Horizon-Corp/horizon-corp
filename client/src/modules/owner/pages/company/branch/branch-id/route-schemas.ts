33d5e18cef9664eafbb179f370c4380e3acdafb2
Jervx <louellagracechua@gmail.com>
2025-01-29 14:52:25 +0800

    feat: Replaced branches table and fix route-schema

--- client/src/modules/owner/pages/company/branch/branch-id/route-schemas.ts
+++ client/src/modules/owner/pages/company/branch/branch-id/route-schemas.ts
@@ -1,7 +1,7 @@
 import z from 'zod'
 
 export const ownerIdPathSchema = z.object({
-    branchId: z.z.string({
+    branchId: z.string({
         required_error: 'owner ID is required',
         invalid_type_error: 'Invalid Owner ID',
     }),
