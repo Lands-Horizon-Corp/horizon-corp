cb471ccd867c68b9de22976ff95304ea10223b2d
Jervx <louellagracechua@gmail.com>
2025-01-31 14:51:16 +0800

    feat: Add employee footstep and member footstep

--- client/src/modules/owner/pages/users/employees/employee-footsteps.tsx
+++ client/src/modules/owner/pages/users/employees/employee-footsteps.tsx
@@ -1,11 +1,24 @@
+import FootstepTable from '@/components/tables/footsteps-table'
 import PageContainer from '@/components/containers/page-container'
 import EnsureOwnerCompany from '@/modules/owner/components/ensure-company'
 
 const OwnerEmployeeFootstepsPage = () => {
     return (
         <PageContainer>
-            <EnsureOwnerCompany>
-
+            <EnsureOwnerCompany disabled>
+                <FootstepTable
+                    className="min-h-[90vh] w-full"
+                    mode="team"
+                    defaultFilter={{
+                        accountType: {
+                            mode: 'equal',
+                            value: 'Employee',
+                            dataType: 'text',
+                            isStaticFilter: true,
+                            displayText: 'Account Type',
+                        },
+                    }}
+                />
             </EnsureOwnerCompany>
         </PageContainer>
     )
--- client/src/modules/owner/pages/users/members/members-activity.tsx
+++ client/src/modules/owner/pages/users/members/members-activity.tsx
@@ -1,10 +1,25 @@
+import FootstepTable from '@/components/tables/footsteps-table'
 import PageContainer from '@/components/containers/page-container'
 import EnsureOwnerCompany from '@/modules/owner/components/ensure-company'
 
 const OwnerMembersActivityPage = () => {
     return (
         <PageContainer>
-            <EnsureOwnerCompany></EnsureOwnerCompany>
+            <EnsureOwnerCompany disabled>
+                <FootstepTable
+                    className="min-h-[90vh] w-full"
+                    mode="team"
+                    defaultFilter={{
+                        accountType: {
+                            mode: 'equal',
+                            value: 'Member',
+                            dataType: 'text',
+                            isStaticFilter : true,
+                            displayText: 'Account Type',
+                        },
+                    }}
+                />
+            </EnsureOwnerCompany>
         </PageContainer>
     )
 }
