5b2038d4535fdc13770bd695bf388a1d35d9e52e
dctrrohan <dctrrohan@gmail.com>
2025-02-06 09:22:26 +0800

    feat: add view accounts table

--- client/src/modules/owner/pages/accounting/accounts-page.tsx
+++ client/src/modules/owner/pages/accounting/accounts-page.tsx
@@ -1,7 +1,15 @@
 import PageContainer from '@/components/containers/page-container'
+import AccountsTable from '@/components/tables/accounts-table'
 
 const AccountsPage = () => {
-    return <PageContainer>ap</PageContainer>
+    return (
+        <PageContainer>
+                <AccountsTable
+                    className="min-h-[90vh] w-full"
+                    actionComponent={({ row }) => <></>}
+                />
+        </PageContainer>
+    )
 }
 
 export default AccountsPage
