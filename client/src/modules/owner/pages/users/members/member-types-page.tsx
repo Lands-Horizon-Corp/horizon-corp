898bf6626a696cd9206b9e5881d32214940c4fdf
Jervx <louellagracechua@gmail.com>
2025-02-06 16:08:17 +0800

    feat: Add create member type form

--- client/src/modules/owner/pages/users/members/member-types-page.tsx
+++ client/src/modules/owner/pages/users/members/member-types-page.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react'
+
 import PageContainer from '@/components/containers/page-container'
 import MemberTypeTable from '@/components/tables/member-type-table'
 import EnsureOwnerCompany from '@/modules/owner/components/ensure-company'
+import { MemberTypeCreateFormModal } from '@/components/forms/member-forms/member-type-create-form'
 
 const OwnerViewMembersPage = () => {
+    const [modalState, setModalState] = useState(false)
+
     return (
         <PageContainer>
             <EnsureOwnerCompany disabled>
+                <MemberTypeCreateFormModal
+                    open={modalState}
+                    onOpenChange={setModalState}
+                />
                 <MemberTypeTable
                     toolbarProps={{
                         createActionProps: {
-                            onClick: () => {},
+                            onClick: () => setModalState(true),
                         },
                     }}
                     className="min-h-[90vh] w-full"
