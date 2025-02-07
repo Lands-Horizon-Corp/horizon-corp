b877170c046516ff4451bf661f1e9a958e435ddc
dctrrohan <dctrrohan@gmail.com>
2025-02-07 08:51:50 +0800

    WIP: accounting create forms

--- client/src/modules/owner/pages/accounting/accounts-page.tsx
+++ client/src/modules/owner/pages/accounting/accounts-page.tsx
@@ -1,10 +1,23 @@
 import PageContainer from '@/components/containers/page-container'
+import { AccountsCreateFormModal } from '@/components/forms/accounts-forms/accounts-create-form'
 import AccountsTable from '@/components/tables/accounts-table'
+import { useState } from 'react'
 
 const AccountsPage = () => {
+    const [createModal, setCreateModal] = useState(true)
+
     return (
         <PageContainer>
+            <AccountsCreateFormModal
+                open={createModal}
+                onOpenChange={setCreateModal}
+            />
             <AccountsTable
+                toolbarProps={{
+                    createActionProps: {
+                        onClick: () => setCreateModal(true),
+                    },
+                }}
                 className="min-h-[90vh] w-full"
                 actionComponent={() => <></>}
             />
---
+++ client/src/validations/form-validation/accounts-schema.ts
@@ -0,0 +1,76 @@
+import { z } from 'zod';
+
+export const TAccountingAccountsEnum = z.enum([
+    'Deposit',
+    'Loan',
+    'A/R-Ledger',
+    'A/R-Aging',
+    'Fines',
+    'Interest',
+    'SVF-Ledger',
+    'W-Off',
+    'A/P-Ledger',
+    'Other'
+]);
+
+export const TEarnedUnearnedInterestEnum = z.enum([
+    'By Formula',
+    'By Formula + Actual Pay',
+    'By Advance Int. + Actual Pay',
+    'None'
+]);
+
+export const TOtherAccountInformationEnum = z.enum([
+    'None',
+    'Jewelry',
+    'Field',
+    'Grocery',
+    'Track Loan Ded',
+    'CIB/CIK Acct.',
+    'COH Acct.'
+]);
+
+export const AccountRequestSchema = z.object({
+    id: z.string(),
+    companyId: z.string(),
+    accountCode:  z.string().min(5, 'Account Code is required'),
+    description:  z.string().min(1, 'Description is required'),
+    altDescription: z.string().optional(),
+    type:TAccountingAccountsEnum.default('Deposit'),
+    maxAmount: z.coerce.number().optional(),
+    minAmount: z.coerce.number().optional(),
+    computationType: z.string().optional(),
+    headerRow: z.coerce.number().optional(),
+    centerRow: z.coerce.number().optional(),
+    totalRow: z.coerce.number().optional(),
+    print: z.boolean().optional(),
+    addOn: z.boolean().optional(),
+    allowRebate: z.boolean().optional(),
+    taxable: z.boolean().optional(),
+    finesAmort: z.coerce.number().optional(),
+    finesMaturity: z.coerce.number().optional(),
+    interestStandard: z.coerce.number().optional(),
+    interestSecured: z.coerce.number().optional(),
+    schemeNo: z.coerce.number().optional(),
+    altCode: z.coerce.number().optional(),
+    glCode: z.coerce.number().optional(),
+    finesGpAmort: z.coerce.number().optional(),
+    addtlGp: z.string().optional(),
+    noGracePeriodDaily: z.boolean(),
+    finesGpMaturity: z.coerce.number().optional(),
+    earnedUnearnedInterest: TEarnedUnearnedInterestEnum.optional(),
+    otherInformationOfAnAccount: TOtherAccountInformationEnum.optional()
+});
+
+export const AccountResourceSchema = AccountRequestSchema.extend({
+    createdAt: z.string(),
+    updatedAt: z.string(),
+    deletedAt: z.string().nullable().optional()
+});
+
+export const AccountPaginatedResourceSchema = z.object({
+    data: z.array(AccountResourceSchema),
+    total: z.number(),
+    page: z.number(),
+    perPage: z.number(),
+});
