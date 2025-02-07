4018b5e6fe7cb7a9bc66a205af33dc18e939042a
Jervx <louellagracechua@gmail.com>
2025-02-07 09:18:51 +0800

    fix: Fix merge conflict

--- client/src/components/tables/feedback-table/index.tsx
+++ client/src/components/tables/feedback-table/index.tsx
@@ -16,12 +16,6 @@ import FeedbackColumns, {
     IFeedbackTableColumnProps,
 } from './column'
 
-<<<<<<< HEAD
-import useDatableFilterState from '@/hooks/use-filter-state'
-import FilterContext from '@/contexts/filter-context/filter-context'
-
-=======
->>>>>>> 59-feature/admin
 import { TableProps } from '../types'
 import { IFeedbackResource } from '@/server/types'
 
@@ -29,13 +23,8 @@ import { cn } from '@/lib'
 import { usePagination } from '@/hooks/use-pagination'
 import useDatableFilterState from '@/hooks/use-filter-state'
 import FeedbackService from '@/server/api-service/feedback-service'
-<<<<<<< HEAD
-import useDataTableState from '@/hooks/data-table-hooks/use-datatable-state'
-import { usePagination } from '@/hooks/use-pagination'
-=======
 import FilterContext from '@/contexts/filter-context/filter-context'
 import useDataTableState from '@/hooks/data-table-hooks/use-datatable-state'
->>>>>>> 59-feature/admin
 import { useDataTableSorting } from '@/hooks/data-table-hooks/use-datatable-sorting'
 
 const data: IFeedbackResource[] = [
