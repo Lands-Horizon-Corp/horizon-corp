20a0b103e32ebcabd7d705871c65bb3bd1a34904
Jervx <louellagracechua@gmail.com>
2025-02-07 13:53:49 +0800

    feat: Add genders page

--- client/src/modules/owner/pages/users/genders-page.tsx
+++ client/src/modules/owner/pages/users/genders-page.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react'
+
+import GenderTable from '@/components/tables/genders-table'
 import PageContainer from '@/components/containers/page-container'
+import { GenderCreateUpdateFormModal } from '@/components/forms/gender-forms/gender-create-update-form'
+import GenderTableOwnerAction from '@/components/tables/genders-table/row-actions/gender-table-owner-action'
 
 const GenderPage = () => {
+    const [createModal, setCreateModal] = useState(false)
+
     return (
         <div>
             <PageContainer>
-                Gender Page
+                <GenderCreateUpdateFormModal
+                    open={createModal}
+                    onOpenChange={setCreateModal}
+                />
+                <GenderTable
+                    toolbarProps={{
+                        createActionProps: {
+                            onClick: () => setCreateModal(true),
+                        },
+                    }}
+                    actionComponent={(props) => (
+                        <GenderTableOwnerAction {...props} />
+                    )}
+                    className="min-h-[90vh] w-full"
+                />
             </PageContainer>
         </div>
     )
