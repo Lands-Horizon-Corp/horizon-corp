ac6b1f901ea8030a866ae01a85aa17a37334aacc
Jervx <louellagracechua@gmail.com>
2025-02-10 21:28:56 +0800

    feat: Add owner action to member educational attainment table

--- client/src/modules/owner/pages/users/members/member-educational-attainment-page.tsx
+++ client/src/modules/owner/pages/users/members/member-educational-attainment-page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import PageContainer from '@/components/containers/page-container'
 import MemberEducationalAttainmentTable from '@/components/tables/member-educational-attainment-table'
 import { MemberEducationalAttainmentCreateUpdateFormModal } from '@/components/forms/member-forms/member-educational-attainment-create-update-form'
+import MemberEducationalAttainmentTableOwnerAction from '@/components/tables/member-educational-attainment-table/row/member-educational-attainment-owner-action'
 
 const OwnerMemberEducationalAttainmentPage = () => {
     const [createModal, setCreateModal] = useState(false)
@@ -15,6 +16,9 @@ const OwnerMemberEducationalAttainmentPage = () => {
             />
             <MemberEducationalAttainmentTable
                 className="min-h-[90vh] w-full"
+                actionComponent={(props) => (
+                    <MemberEducationalAttainmentTableOwnerAction {...props} />
+                )}
                 toolbarProps={{
                     createActionProps: { onClick: () => setCreateModal(true) },
                 }}
