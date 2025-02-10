8d4bead39f8d44d667e9091d3b9ad50565451b7c
Jervx <louellagracechua@gmail.com>
2025-02-10 15:54:33 +0800

    chore: Improve forms

--- client/src/modules/owner/pages/users/members/member-educational-attainment-page.tsx
+++ client/src/modules/owner/pages/users/members/member-educational-attainment-page.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+
 import PageContainer from '@/components/containers/page-container'
 import MemberEducationalAttainmentTable from '@/components/tables/member-educational-attainment-table'
+import { MemberEducationalAttainmentCreateUpdateFormModal } from '@/components/forms/member-forms/member-educational-attainment-create-update-form'
 
 const OwnerMemberEducationalAttainmentPage = () => {
+    const [createModal, setCreateModal] = useState(false)
+
     return (
         <PageContainer>
-            <MemberEducationalAttainmentTable className="w-full" />
+            <MemberEducationalAttainmentCreateUpdateFormModal
+                open={createModal}
+                onOpenChange={setCreateModal}
+            />
+            <MemberEducationalAttainmentTable
+                className="min-h-[90vh] w-full"
+                toolbarProps={{
+                    createActionProps: { onClick: () => setCreateModal(true) },
+                }}
+            />
         </PageContainer>
     )
 }
