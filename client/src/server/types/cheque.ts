211b719f1e880435be8f08c295e34013eed58c38
dctrrohan <dctrrohan@gmail.com>
2025-02-14 12:21:30 +0800

    wip: accounts check hooks and types

--- client/src/server/types/cheque.ts
+++ client/src/server/types/cheque.ts
@@ -1,10 +1,9 @@
 import { TEntityId } from "./common";
 
 export type Cheque = {
-    id: TEntityId;
-    bank: string;
-    check_date: Date;
-    media_id: TEntityId;
-    description: string;
-  };
-  
+  id: TEntityId;
+  bank: string;
+  check_date: Date;
+  media_id: TEntityId;
+  description: string;
+};
