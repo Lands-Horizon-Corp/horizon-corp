20d915b54ad71b22b8083339ce4ef7deaceefb4a
dctrrohan <dctrrohan@gmail.com>
2025-02-14 12:21:30 +0800

    wip: accounts check hooks and types

--- client/src/server/api-service/transactions/accounting-cheque.ts
+++ client/src/server/api-service/transactions/accounting-cheque.ts
@@ -1,7 +1,6 @@
 import { TEntityId } from "@/server/types";
 import APIService from "../api-service";
 import qs from "query-string";
-import { downloadFile } from "@/server/helpers";
 import { Cheque } from "@/server/types/cheque";
 
 export default class ChequeService {
@@ -80,31 +79,5 @@ export default class ChequeService {
         const endpoint = `${ChequeService.BASE_ENDPOINT}/bulk-delete`;
         const payload = { ids };
         await APIService.delete<void>(endpoint, payload);
-    }
-
-    public static async exportAll(): Promise<void> {
-        const url = this.buildUrl('/export', {});
-        await downloadFile(url, 'all_cheques_export.xlsx');
-    }
-
-    public static async exportAllFiltered(filters?: string): Promise<void> {
-        const url = this.buildUrl(`/export-search?filter=${filters || ''}`, {});
-        await downloadFile(url, 'filtered_cheques_export.xlsx');
-    }
-
-    public static async exportSelected(ids: TEntityId[]): Promise<void> {
-        const url = qs.stringifyUrl(
-            {
-                url: `${ChequeService.BASE_ENDPOINT}/export-selected`,
-                query: { ids },
-            },
-            { skipNull: true }
-        );
-        await downloadFile(url, 'selected_cheques_export.xlsx');
-    }
-
-    public static async exportCurrentPage(page: number): Promise<void> {
-        const url = this.buildUrl(`/export-current-page/${page}`, {});
-        await downloadFile(url, `current_page_cheques_${page}_export.xlsx`);
     }
 }
