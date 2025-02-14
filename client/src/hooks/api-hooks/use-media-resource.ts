6ce49a65261803ac3fcb08b548fd2cb463e76946
Jervx <louellagracechua@gmail.com>
2025-02-15 00:31:22 +0800

    feat: Add ETA onchange event for media single image upload

--- client/src/hooks/api-hooks/use-media-resource.ts
+++ client/src/hooks/api-hooks/use-media-resource.ts
@@ -8,11 +8,13 @@ import { serverRequestErrExtractor } from '@/helpers'
 import MediaService from '@/server/api-service/media-service'
 
 export const useSinglePictureUpload = ({
-    onUploadProgressChange,
-    onSuccess,
     onError,
+    onSuccess,
+    onUploadETAChange,
+    onUploadProgressChange,
 }: {
     onUploadProgressChange?: (progress: number) => void
+    onUploadETAChange?: (eta: string) => void
 } & IOperationCallbacks<IMediaResource, string>) => {
     return useMutation<IMediaResource, string, File>({
         mutationKey: ['upload-media-photo'],
@@ -25,6 +27,8 @@ export const useSinglePictureUpload = ({
 
             onUploadProgressChange?.(0)
 
+            const startTime = Date.now()
+
             const [error, data] = await withCatchAsync(
                 MediaService.upload(fileImage, (progressEvent) => {
                     if (!progressEvent.total) return
@@ -34,6 +38,18 @@ export const useSinglePictureUpload = ({
                     )
 
                     onUploadProgressChange?.(progress)
+
+                    const elapsedTime = (Date.now() - startTime) / 1000
+                    const uploadSpeed = progressEvent.loaded / elapsedTime
+                    const remainingBytes =
+                        progressEvent.total - progressEvent.loaded
+                    const etaSeconds =
+                        uploadSpeed > 0 ? remainingBytes / uploadSpeed : 0
+                    const minutes = Math.floor(etaSeconds / 60)
+                    const seconds = Math.round(etaSeconds % 60)
+                    const etaFormatted = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+
+                    onUploadETAChange?.(etaFormatted)
                 })
             )
 
