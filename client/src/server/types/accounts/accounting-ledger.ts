e8d9d27cc26852e9a6cd290f9797569d8c26cb27
dctrrohan <dctrrohan@gmail.com>
2025-02-14 14:34:59 +0800

    feat: add paginated types on accounting ledger

--- client/src/server/types/accounts/accounting-ledger.ts
+++ client/src/server/types/accounts/accounting-ledger.ts
@@ -1,4 +1,5 @@
-import { TEntityId } from "../common";
+import { ITimeStamps, TEntityId } from "../common";
+import { IPaginatedResult } from "../paginated-result";
 
 export type AccountingLedger = {
   id: TEntityId;
@@ -20,6 +21,29 @@ export type AccountingLedger = {
   created_by: TEntityId;
   created_at: Date;
 };
+
+export interface IAccountingLedgerResource extends ITimeStamps {
+  id: TEntityId;
+  description: string;
+  notes: string;
+  company_id: TEntityId;
+  member_profile_id: TEntityId;
+  account_id: TEntityId;
+  employee_id: TEntityId;
+  or_number: string;
+  transaction_source: TransactionSource;
+  debit: number;
+  credit: number;
+  balance: Date;
+  transaction_date: Date;
+  entry_date: Date;
+  line_number?: number | null;
+  transaction_type_id: string;
+  created_by: TEntityId;
+  created_at: Date;
+}
+
+export type IAccountingLedgerPaginatedResource = IPaginatedResult<IAccountingLedgerResource>
 
 export enum TransactionSource {
   withdrawal = "withdrawal",
