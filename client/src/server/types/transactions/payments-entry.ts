9987223293d0dcabcbb8d2c83e2f2e6a3c6772ed
dctrrohan <dctrrohan@gmail.com>
2025-02-17 14:40:49 +0800

    chore: make notes optional

---
+++ client/src/server/types/transactions/payments-entry.ts
@@ -0,0 +1,17 @@
+import { TEntityId } from '../common'
+
+export interface IPaymentsEntryRequest {
+    memberId: TEntityId
+    ORNumber: string
+    amount: number
+    accountsId: TEntityId
+    transactionType: TEntityId
+    isPrinted: boolean
+    notes?: string
+}
+
+export interface IPaymentsEntryResource extends IPaymentsEntryRequest {
+    id: TEntityId
+    createdAt: Date
+    updatedAt: Date
+}
