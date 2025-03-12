7731cad965e1e5785d597172ba8dba5965b3db32
Jervx <louellagracechua@gmail.com>
2025-03-12 14:52:03 +0800

    chore: Purged other modules dependency to sidebar component
    Changes to all modules/ should be in their respective branches

--- client/src/modules/admin/pages/members/view-members.tsx
+++ client/src/modules/admin/pages/members/view-members.tsx
@@ -1,10 +1,6 @@
-import MembersTable from '@/components/tables/members-table'
-
 const AdminViewMembersPage = () => {
     return (
-        <div className="flex w-full max-w-full flex-col items-center px-4 pb-6 sm:px-8">
-            <MembersTable className="min-h-[90vh] w-full" />
-        </div>
+        <div className="flex w-full max-w-full flex-col items-center px-4 pb-6 sm:px-8"></div>
     )
 }
 
