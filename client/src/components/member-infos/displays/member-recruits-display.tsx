0ec5f7330673661b0d71488be88b19951f4ab5bf
Jervx <louellagracechua@gmail.com>
2025-03-24 15:46:10 +0800

    feat(member-info): Add member info display

---
+++ client/src/components/member-infos/displays/member-recruits-display.tsx
@@ -0,0 +1,117 @@
+import DOMPurify from 'dompurify'
+
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@/components/ui/accordion'
+
+import { IMemberRecruitsResource } from '@/server'
+import ImageDisplay from '@/components/image-display'
+import CopyTextButton from '@/components/copy-text-button'
+
+interface Props {
+    recruits?: IMemberRecruitsResource[]
+}
+
+const MemberRecruitsDisplay = ({ recruits }: Props) => {
+
+    return (
+        <div className="space-y-4">
+            <p>Member Recruits</p>
+            <p className="!mt-0 text-sm text-muted-foreground/60">
+                Members recruited under this account
+            </p>
+            {(!recruits || recruits.length === 0) && (
+                <p className="w-full text-center text-xs text-muted-foreground/70">
+                    No recruits found
+                </p>
+            )}
+            {recruits?.map((recruit) => (
+                <div
+                    key={recruit.id}
+                    className="space-y-2 rounded-xl bg-secondary/20 p-4"
+                >
+                    <div className="flex items-center gap-x-4">
+                        <ImageDisplay
+                            src={
+                                recruit.membersProfileRecruited?.media
+                                    ?.downloadURL
+                            }
+                            className="size-16 rounded-xl"
+                        />
+                        <div className="grid flex-1 gap-2 md:grid-cols-5">
+                            <div className="space-y-2">
+                                <p>
+                                    {`${recruit.membersProfileRecruited?.firstName ?? '-'} ${recruit.membersProfileRecruited?.middleName ?? '-'} ${recruit.membersProfileRecruited?.lastName ?? '-'} ${recruit.membersProfileRecruited?.suffix ?? ''}`}
+                                </p>
+                                <p className="text-xs text-muted-foreground/70">
+                                    Full Name
+                                </p>
+                            </div>
+
+                            <div className="space-y-2">
+                                <p>
+                                    {recruit.membersProfileRecruited
+                                        ?.passbookNumber ?? '-'}
+                                </p>
+                                <p className="text-xs text-muted-foreground/70">
+                                    Passbook Number{' '}
+                                    {recruit.membersProfileRecruited
+                                        ?.passbookNumber && (
+                                        <CopyTextButton
+                                            successText="Passbook number copied"
+                                            textContent={
+                                                recruit.membersProfileRecruited
+                                                    .passbookNumber
+                                            }
+                                        />
+                                    )}
+                                </p>
+                            </div>
+
+                            <div className="space-y-2">
+                                <p className="truncate">
+                                    {recruit.membersProfileRecruited?.memberType
+                                        ?.name ?? '-'}
+                                </p>
+                                <p className="text-xs text-muted-foreground/70">
+                                    Member Type
+                                </p>
+                            </div>
+
+                            <div className="space-y-2">
+                                <p>{recruit.dateRecruited}</p>
+                                <p className="text-xs text-muted-foreground/70">
+                                    Date Recruited
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <Accordion collapsible type="single" className="w-full">
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger className="font-normal">
+                                Description
+                            </AccordionTrigger>
+                            <AccordionContent className="prose-h1: prose w-full !max-w-full rounded-xl p-4 text-sm text-foreground/70 dark:prose-invert prose-p:text-foreground/80 prose-strong:text-foreground sm:text-sm">
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: DOMPurify.sanitize(
+                                            recruit.description &&
+                                                recruit.description.length > 0
+                                                ? recruit.description
+                                                : '<i>No Description</i>'
+                                        ),
+                                    }}
+                                />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MemberRecruitsDisplay
