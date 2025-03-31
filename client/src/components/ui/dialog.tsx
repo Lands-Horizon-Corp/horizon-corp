6654421a008bd25de7fb7f4475f23f595b9b2fd8
Jervx <louellagracechua@gmail.com>
2025-03-31 21:47:06 +0800

    style: Fix command dialog styling no blur

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
+import * as DialogPrimitive from '@radix-ui/react-dialog'
 
 import { cn } from '@/lib/utils'
 
@@ -51,37 +51,39 @@ const DialogContent = React.forwardRef<
             ...props
         },
         ref
-    ) => (
-        <DialogPortal>
-            <DialogOverlay
-                className={cn(
-                    'bg-black/30 dark:bg-background/60',
-                    overlayClassName
-                )}
-            />
-            <DialogPrimitive.Content
-                ref={ref}
-                className={cn(
-                    'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-popover/90 p-6 shadow-lg backdrop-blur duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
-                    className
-                )}
-                {...props}
-            >
-                {children}
-                {!hideCloseButton && (
-                    <DialogPrimitive.Close
-                        className={cn(
-                            'absolute right-4 top-4 size-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
-                            closeButtonClassName
-                        )}
-                    >
-                        <X className="size-full" />
-                        <span className="sr-only">Close</span>
-                    </DialogPrimitive.Close>
-                )}
-            </DialogPrimitive.Content>
-        </DialogPortal>
-    )
+    ) => {
+        return (
+            <DialogPortal>
+                <DialogOverlay
+                    className={cn(
+                        'bg-black/30 dark:bg-background/60',
+                        overlayClassName
+                    )}
+                />
+                <DialogPrimitive.Content
+                    ref={ref}
+                    className={cn(
+                        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-popover/90 p-6 shadow-lg backdrop-blur duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+                        className
+                    )}
+                    {...props}
+                >
+                    {children}
+                    {!hideCloseButton && (
+                        <DialogPrimitive.Close
+                            className={cn(
+                                'absolute right-4 top-4 size-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
+                                closeButtonClassName
+                            )}
+                        >
+                            <X className="size-full" />
+                            <span className="sr-only">Close</span>
+                        </DialogPrimitive.Close>
+                    )}
+                </DialogPrimitive.Content>
+            </DialogPortal>
+        )
+    }
 )
 DialogContent.displayName = DialogPrimitive.Content.displayName
 
